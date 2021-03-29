1608dcd0e81a3e33176c981491311dadae384437
introvertach <76014743+introvertach@users.noreply.github.com>
2021-03-30 02:35:15 +0300

    Add files via upload

--- Filter.cpp
+++ Filter.cpp
@@ -30,3 +30,53 @@ QColor InvertFilter::calcNewPixelColor(const QImage& img, int x, int y) const
 	color.setRgb(255 - color.red(), 255 - color.green(), 255 - color.blue());
 	return color;
 }
+
+QColor MatrixFilter::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	float ReturnR = 0;
+	float ReturnG = 0;
+	float ReturnB = 0;
+	int radius = mKernel.getRadius();
+	int size = mKernel.getSize();
+
+	for(int i = -radius; i <= radius; i++)
+		for (int j = -radius; j <= radius; j++)
+		{
+			int idx = (i + radius) * size + (j + radius);
+			QColor color = img.pixelColor(clamp(x + j, img.width() - 1, 0), 
+				clamp(y + i, img.height() - 1, 0));
+
+			ReturnR += color.red() * mKernel[idx];
+			ReturnG += color.green() * mKernel[idx];
+			ReturnB += color.blue() * mKernel[idx];
+		}
+	return QColor(clamp(ReturnR, 255.f, 0.f), clamp(ReturnG, 255.f, 0.f), 
+		clamp(ReturnB, 255.f, 0.f));
+}
+
+QColor GrayScaleFilter::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	int Intensity = 0.299 * color.red() + 0.587 * color.green() + 0.114 * color.blue();
+	color.setRgb(Intensity, Intensity, Intensity);
+	return color;
+}
+
+QColor SepiaFilter::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	float Intensity = 0.299 * color.red() + 0.587 * color.green() + 0.114 * color.blue();
+	int k = 40;
+	color.setRgb(clamp(Intensity + 2 * k, 255.f, 0.f), clamp(Intensity + 0.5f * k, 255.f, 0.f),
+		clamp(Intensity - k, 255.f, 0.f));
+	return color;
+}
+
+QColor IncreaseBrightnessFilter::calcNewPixelColor(const QImage& img, int x, int y) const
+{
+	QColor color = img.pixelColor(x, y);
+	float k = 50;
+	color.setRgb(clamp(color.red() + k, 255.f, 0.f), clamp(color.green() + k, 255.f, 0.f),
+		clamp(color.blue() + k, 255.f, 0.f));
+	return color;
+}
--- Filter.h
+++ Filter.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<qimage.h>
 #include<memory>
+#include<iostream>
+#include<cstdlib>
 class Filter
 {
 	protected:
@@ -38,3 +40,128 @@ public:
 	float& operator [] (std::size_t id) { return data[id]; }
 
 };
+
+class MatrixFilter : public Filter
+{
+protected:
+	Kernel mKernel;
+	QColor calcNewPixelColor(const QImage& img, int x, int y) const override;
+public:
+	MatrixFilter(const Kernel& kernel) : mKernel(kernel) {};
+	virtual ~MatrixFilter() = default;
+};
+
+class BlurKernel : public Kernel
+{
+public:
+	using Kernel::Kernel;
+	BlurKernel(std::size_t radius = 2) : Kernel(radius)
+	{
+		for (std::size_t i = 0; i < getLen(); i++)
+			data[i] = 1.f / getLen();
+	}
+};
+
+class BlurFilter : public MatrixFilter
+{
+public:
+	BlurFilter(std::size_t radius = 1) : MatrixFilter(BlurKernel(radius)) {}
+};
+
+class GaussinKernel : public Kernel
+{
+public:
+	using Kernel::Kernel;
+	GaussinKernel(std::size_t radius = 2, float sigma = 3.f) : Kernel(radius)
+	{
+		float norm = 0;
+		int signed_radius = static_cast<int>(radius);
+
+		for(int i = -signed_radius; i <= signed_radius; i++)
+			for (int j = -signed_radius; j <= signed_radius; j++)
+			{
+				std::size_t idx = (i + radius) * getSize() + (j + radius);
+				data[idx] = std::exp( -(i * i + j * j) / (sigma * sigma) );
+				norm += data[idx];
+			}
+
+		for (std::size_t idx = 0; idx < getLen(); idx++)
+			data[idx] /= norm;
+	}
+};
+
+class GaussinFilter : public MatrixFilter
+{
+public:
+	GaussinFilter(std::size_t radius = 1) : MatrixFilter(GaussinKernel(radius)) {}
+};
+
+class GrayScaleFilter : public Filter
+{
+	QColor calcNewPixelColor(const QImage& img, int x, int y) const override;
+};
+
+class SepiaFilter : public Filter
+{
+	QColor calcNewPixelColor(const QImage& img, int x, int y) const override;
+};
+
+class IncreaseBrightnessFilter : public Filter
+{
+	QColor calcNewPixelColor(const QImage& img, int x, int y) const override;
+};
+
+class ySobelKernel : public Kernel
+{
+public:
+	using Kernel::Kernel;
+	ySobelKernel(std::size_t radius = 2) : Kernel(radius)
+	{
+		data[0] = -1, data[1] = -2, data[2] = -1, 
+			data[3] = 0, data[4] = 0, data[5] = 0,
+			data[6] = 1, data[7] = 2, data[8] = 1;
+	}
+};
+
+class ySobelFilter : public MatrixFilter
+{
+public:
+	ySobelFilter(std::size_t radius = 2) : MatrixFilter(ySobelKernel(radius)) {}
+};
+
+class xSobelKernel : public Kernel
+{
+public:
+	using Kernel::Kernel;
+	xSobelKernel(std::size_t radius = 2) : Kernel(radius)
+	{
+		data[0] = -1, data[1] = 0, data[2] = 1,
+			data[3] = -2, data[4] = 0, data[5] = 2,
+			data[6] = -1, data[7] = 0, data[8] = 1;
+	}
+};
+
+class xSobelFilter : public MatrixFilter
+{
+public:
+	xSobelFilter(std::size_t radius = 2) : MatrixFilter(xSobelKernel(radius)) {}
+};
+
+class SharpnessKernel : public Kernel
+{
+public:
+	using Kernel::Kernel;
+	SharpnessKernel(std::size_t radius = 2) : Kernel(radius)
+	{
+		data[0] = 0, data[1] = -1, data[2] = 0,
+			data[3] = -1, data[4] = 5, data[5] = -1,
+			data[6] = 0, data[7] = -1, data[8] = 0;
+	}
+};
+
+class SharpnessFilter : public MatrixFilter
+{
+public:
+	SharpnessFilter(std::size_t radius = 2) : MatrixFilter(SharpnessKernel(radius)) {}
+};
+
